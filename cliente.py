10696a79c8f87be1b83a3a00ca3ee7d68c88eac1
krolikpt <krolikpt@gmail.com>
2021-04-23 14:53:56 +0100

    commit

--- cliente.py
+++ cliente.py
@@ -5,46 +5,46 @@ import time
 import sys
 
 # Definição das variáveis
-host = ""                                               # Exemplo: 192.168.1.20
-port = 7777
-buffer_size = 2048
+host = "192.168.1.20"                                                   # Exemplo: 192.168.1.20
+port = 7777                                                             # Número da porta que vai ser usada para a comunicação
+buffer_size = 2048                                                      # Tamanho da memória utilizada para os dados
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Criação de um objeto socket
-s.connect((host, port))                                 # Uso do connect para fazer a conexão com host e porta
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                   # Criação de um objeto socket que irá funcionar com IPv4 em modo TCP
+s.connect((host, port))                                                 # Uso do connect para fazer a conexão com host e porta
 
-nickname = input("Nickname: ")                          
+nickname = input("Nickname: ")                                          # Input para o utilizador colocar o seu nickname
 
 
-def receive():
-    while True:
+def receive():                                                          # Função para receber os dados dos clientes
+    while True:                                                         # Ciclo que vai estar a receber os dados dos clientes
         try:
-            mensagem = s.recv(buffer_size).decode()
+            mensagem = s.recv(buffer_size).decode()                     # Criação de uma variável que vai guardar a infomação vinda do cliente
             
-            if mensagem != "":
+            if mensagem != "":                                          # Se o valor dos bytes for diferente de vazio ele mostra a mensagem no terminal
                 print(mensagem)
                 
-            time.sleep(0.2)
+            time.sleep(0.2)                                             # Delay da Thread
         except:
-            print("Servidor desligado!")
-            s.close()
-            break
+            print("Servidor desligado!")                                # Mostra a mensagem quando o servidor está desligado
+            s.close()                                                   # Fecha a conexão
+            break                                                       # Interrompe o ciclo
 
 
-def write():
-    while True:
+def write():                                                            # Função para enviar os dados para o servidor
+    while True:                                                         # Ciclo que vai estar a enviar as mensagens
         try:
-            mensagem = input("")
+            mensagem = input("")                                        # Mensagem para o servidor
     
-            mensagem_bytes = nickname + ": " + mensagem
-            s.send(mensagem_bytes.encode())
-            time.sleep(0.2)
+            mensagem_bytes = nickname + ": " + mensagem                 # Variável que vai concatenar o nickname do utilizador com a mensagem escrita
+            s.send(mensagem_bytes.encode())                             # Conversão da mensagem em string para bytes para enviar para o servidor
+            time.sleep(0.2)                                             # Delay da Thread 
         except:
-            s.close()
-            sys.exit(0)
+            s.close()                                                   # Fecha a conexão
+            sys.exit(0)                                                 # Fecha a aplicação
 
 
-receive_thread = threading.Thread(name="receive", target=receive)
-receive_thread.start()
+receive_thread = threading.Thread(name="receive", target=receive)       # Criação da thread receive() para receber os dados ao mesmo tempo que envia
+receive_thread.start()                                                  # Iniciação da thread receive()
 
-write_thread = threading.Thread(name="write", target=write)
-write_thread.start()
+write_thread = threading.Thread(name="write", target=write)             # Criação da thread write() para enviar os dados ao mesmo tempo que recebe
+write_thread.start()                                                    # Iniciação da thread write()
